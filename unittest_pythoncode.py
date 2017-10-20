e60eb221728c9e30fb975ad31475e58293b62674
gitygity <gity.ghasemi@gmail.com>
2017-10-20 14:45:32 +0330

    final unit test code
    final unit test of bf code

--- unittest_pythoncode.py
+++ unittest_pythoncode.py
@@ -6,8 +6,8 @@ class Test_pythonbf(unittest.TestCase):
         bf_code='+++++++++++11[->+++++++77>++++++++++110>++++++++++110>++++++++++110>+++++++++99>+++++++++99>+++++++++99>+++33>++++++66>+++++++++99>+++++++++99>++++++++++110>+++++++++99>+++++++++99>++++++++++110>+++++++++99>+++++++++99<<<<<<<<<<<<<<<<<]>.>+.>+++++.>++++++.>--.>+++.>--.>-.>-.>+++++.>--.>.>++++.>--.>++++.>+++++.>--.'
         self.assertEqual(gityghasemi.python_bf(bf_code),'Mostafa Ahangarha')
 
-        #bf_code='++++[->++++++++<]-[----->>++>++<<<]>>+++++++.>-.+++.<+++++.>-------.+++.<<.>>.+.+++.------.+++++++.-----.<<+.'
-        #self.assertEqual(gityghasemi.python_bf(bf_code), 'mehrad dehbid!')
+        bf_code='++++[->++++++++<]-[----->>++>++<<<]>>+++++++.>-.+++.<+++++.>-------.+++.<<.>>.+.+++.------.+++++++.-----.<<+.'
+        self.assertEqual(gityghasemi.python_bf(bf_code), 'mehrad dehbid!')
 
         bf_code='>+++++++[<++++++++++> -]<+.print G>> ++++++++[< +++++++++>-]<+.print I>>+++++++++++++++++++++[<++++>-]< .print T>> +++++++++++[< ++++++++> -]<+.print Y>++++++++++++++++++++++++++++++++.print >>+++++++[< ++++++++++> -]<+.print G>> ++++++++[< +++++++++> -]<. print H>> +++++++++++++[< +++++> -]<.print A>> +++++++++++++++++++++[< ++++> -]< -.print S>> +++++++++++++++++++++++[< +++> -]<.print E>> +++++++++++[< +++++++>-]<.print M>>++++++++[<+++++++++>-]<+.print I'
         self.assertEqual(gityghasemi.python_bf(bf_code), 'GITY GHASEMI')
@@ -19,17 +19,9 @@ class Test_pythonbf(unittest.TestCase):
         
                 
         #test underflow pointer
-        bf_code='++++>++++<------.'
+        bf_code='++++>++++<<.'
         self.assertRaises(Exception,gityghasemi.python_bf,bf_code)
-        
-        #test Memory overflow
-        bf_code='+'*256+'.'
-        self.assertRaises(Exception,gityghasemi.python_bf,bf_code)
-        
-        #test Memory underflow
-        bf_code='++++>++'+'<-----.'
-        self.assertRaises(Exception,gityghasemi.python_bf,bf_code)
-        
+              
 
 if __name__ == '__main__':
     unittest.main()
